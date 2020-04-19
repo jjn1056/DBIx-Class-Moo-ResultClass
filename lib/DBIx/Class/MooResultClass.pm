b88a08c719cdf8a69543c0802b884a83d6ad3891
John Napiorkowski <jjn1056@yahoo.com>
2020-04-19 14:39:12 -0500

    more prep for release

--- lib/DBIx/Class/MooResultClass.pm
+++ lib/DBIx/Class/MooResultClass.pm
@@ -23,4 +23,66 @@ sub import {
 
 1;
 
+=head1 TITLE
+
+DBIx::Class::MooResultClass - Moo-ify DBIC Resultclasses
+
+=head1 SYNOPSIS
+
+    package Schema::Result::Artist;
+
+    use DBIx::Class::MooResultClass;
+
+    extends 'Schema::Result';
+    with 'Component';
+
+    has spork => (is => 'ro', default => sub { 'THERE IS NO SPORK' });
+
+    __PACKAGE__->table('artist');
+
+    __PACKAGE__->add_columns(
+      artist_id => {
+        data_type => 'integer',
+        is_auto_increment => 1,
+      },
+      name => {
+        data_type => 'varchar',
+        size => '96',
+      });
+
+    __PACKAGE__->set_primary_key('artist_id');
+
+    my $artist = $schema->resultset('Artist')
+      ->create({name=>'Foo');
+
+    warn $artist->spork; # 'THERE IS NO SPORK'
+
+    my $another = $schema->resultset('Artist')
+      ->create({name=>'Foo', spork=>'foo');
+
+    warn $artist->spork; # 'foo'
+
+=head1 DESCRIPTION
+
+  TBD
+
+=head1 AUTHOR
+ 
+John Napiorkowski L<email:jjnapiork@cpan.org>
+mst
+
+=head1 SEE ALSO
+ 
+L<DBIx::Class>, L<Moo>
+    
+=head1 COPYRIGHT & LICENSE
+ 
+Copyright 2020, John Napiorkowski L<email:jjnapiork@cpan.org>
+ 
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
+
 
